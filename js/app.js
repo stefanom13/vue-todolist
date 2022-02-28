0e358ab53f4c360f0cedef8bde7b588cb3db81b6
stefanom13 <stefanomancuso13@gmail.com>
2022-02-28 12:11:33 +0100

    aggiunta lista trammite js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-console.log('ciaooo');
+// // MILESTONE 1
+// Stampare all’interno di una lista, un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante “aggiungi”:
+// cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo,
+//  che quindi viene aggiunto alla lista dei todo esistenti.
+
+const app = new Vue({
+    el: '#app',
+  
+    data:{
+      newTodo: '',
+      listTodo: [
+        {
+          testo: 'Fare la spesa',
+          done: 'false'
+        },
+        {
+          testo: 'Fare esercizi  boolean',
+          done: 'false'
+        },
+        {
+          testo: 'Passare dal commercialista',
+          done: 'false'
+        },
+      ]
+    },
+  
+    methods: {
+        
+    }
+  })

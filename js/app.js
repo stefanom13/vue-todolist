a84d45e2aa730c92839c20805986cf72b826d1dd
stefanom13 <stefanomancuso13@gmail.com>
2022-02-28 13:46:08 +0100

    completato

--- js/app.js
+++ js/app.js
@@ -18,20 +18,46 @@ const app = new Vue({
       listTodo: [
         {
           testo: 'Fare la spesa',
-          done: 'false'
+          done: false
         },
         {
           testo: 'Fare esercizi  boolean',
-          done: 'false'
+          done: false
         },
         {
           testo: 'Passare dal commercialista',
-          done: 'false'
+          done: false
         },
       ]
     },
   
     methods: {
-        
+        // funzione per aggiungere newTodo nella nostra lista
+        addTodo: function(){
+            if (this.newTodo !== '') {
+                
+                const newTodo = {
+                    testo: this.newTodo,
+                    done: false
+                }
+
+                this.listTodo.push( newTodo );
+            } else {
+                
+            }
+
+            this.newTodo = '';
+        },
+        // funzione per eliminare Todo nella nostra lista
+        deleteTodo ( i ) {
+            this.listTodo.splice(i,1);
+        },
+        toggle: function ( i ){
+            const { done } = this.listTodo[i]
+            // console.log( done);
+
+            this.listTodo [i].done = !done
+            // per modificare il valore booleano
+        }
     }
   })
